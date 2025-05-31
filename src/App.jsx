ef1dcfc37e24e843d29ec8ed189f9247ef8edec1
kishanth12 <kishanth1209@gmail.com>
2025-05-31 11:49:00 +0530

    list page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Brand from "./pages/Brand";
 
 
 export const backendUrl =import.meta.env.VITE_BACKEND_URL;
+export const currency ="$"
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token') || '');  
