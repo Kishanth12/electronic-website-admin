a59e1eaf1a36db3c9c2fc22828a787a83d4079ea
kishanth12 <kishanth1209@gmail.com>
2025-06-15 09:43:13 +0530

    test

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -62,9 +62,9 @@ const List = ({token}) => {
           list.map((item)=>(
             <div className='grid grid-cols-[1fr_3fr_1fr_1fr_1fr_0.5fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr_0.5fr] items-center gap-2 py-1 px-2 border text-sm' key={item._id}>
               <img className='w-12' src={item.image[0]} alt="" />
-              <p>{item.name}</p>
-              <p>{item.category.name}</p>
-              <p>{item.brand.name}</p>
+              <p>{item?.name}</p>
+              <p>{item.category?.name}</p>
+              <p>{item.brand?.name}</p>
               <p>{currency}{item.price}</p>
               <p className='text-right md:text-center cursor-pointer text-lg' onClick={()=>removeProduct(item._id)}>X</p>
             </div>
